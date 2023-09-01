dcc17fb35d7989ac32be6c4e4ed1c72dca8903e6
94-c <hyeongwoo26@gmail.com>
2023-09-01 15:16:04 +0900

    project setting

---
+++ src/test/java/com/fast/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fast.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
