1ef8f25c60c7323384e7cfa48e2e6cad6e62a6d0
94-c <hyeongwoo26@gmail.com>
2023-09-05 17:36:44 +0900

    #1 - data rest 설정

--- src/main/java/com/fast/board/repository/ArticleCommentRepository.java
+++ src/main/java/com/fast/board/repository/ArticleCommentRepository.java
@@ -2,6 +2,7 @@ package com.fast.board.repository;
 
 import com.fast.board.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
