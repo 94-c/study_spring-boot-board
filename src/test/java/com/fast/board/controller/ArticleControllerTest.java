50740ae51c9b97eb62df656d99b8c096e4ac9e2b
94-c <hyeongwoo26@gmail.com>
2023-09-11 09:34:15 +0900

    #16 - 게시글 관련 뷰 테스트 작성

---
+++ src/test/java/com/fast/board/controller/ArticleControllerTest.java
@@ -0,0 +1,66 @@
+package com.fast.board.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("View 컨트롤러 - 게시글")
+@WebMvcTest
+class ArticleControllerTest {
+
+    private final MockMvc mvc;
+    public ArticleControllerTest(MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view][GET} 게시글 리스트 (게시판) 페이지 - 정상 호출 ")
+    @Test
+    public void givenNoting_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles"));
+    }
+
+    @DisplayName("[view][GET} 게시글 상세 페이지 - 정상 호출 ")
+    @Test
+    public void givenNoting_whenRequestingArticlesView_thenReturnsArticleView() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("article"));
+    }
+
+    @DisplayName("[view][GET} 게시글 검색 전용 페이지 - 정상 호출 ")
+    @Test
+    public void givenNoting_whenRequestingArticleSearchView_thenReturnsArticleView() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/articles/search"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+    }
+
+    @DisplayName("[view][GET} 게시글 해시태그 검색 페이지 - 정상 호출 ")
+    @Test
+    public void givenNoting_whenRequestingArticleHashtagSearchView_thenReturnsArticleView() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/articles/search-hashtag"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+    }
+}
