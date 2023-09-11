05a083b1f8974bcf1e6e5a05151974f766bbafab
94-c <hyeongwoo26@gmail.com>
2023-09-11 17:43:42 +0900

    #16 - 게시판 상세 페이지에 decoupled logic 적용

--- src/test/java/com/fast/board/controller/ArticleControllerTest.java
+++ src/test/java/com/fast/board/controller/ArticleControllerTest.java
@@ -3,21 +3,24 @@ package com.fast.board.controller;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 게시글")
-@WebMvcTest
+@RunWith(SpringRunner.class)
+@WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
 
-    private final MockMvc mvc;
-    public ArticleControllerTest(MockMvc mvc) {
-        this.mvc = mvc;
-    }
+    @Autowired
+    private MockMvc mvc;
+
 
     @DisplayName("[view][GET} 게시글 리스트 (게시판) 페이지 - 정상 호출 ")
     @Test
@@ -27,7 +30,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
     }
